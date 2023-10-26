05a8f80aaffa49b5adbc17fc2091d0a414120f48
samedfft2634 <samedfft@gmail.com>
2023-10-26 16:45:19 +0300

    not found

--- script.js
+++ script.js
@@ -6,32 +6,49 @@ const imgContent = document.querySelector(".imgContent p");
 const imgSpan = document.querySelector(".imgContent span");
 const recipeContainer = document.querySelector(".recipeContainer ");
 const recipeMeasure = document.querySelector(".recipeContainer ");
+const content = document.querySelector(".content");
+const ins = document.querySelector(".instructions");
+const recipeBtn = document.querySelector(".recipe-btn");
+const closeBtn = document.querySelector(".fa-times");
+
+content.style.display = "none";
+ins.style.display = "none";
 
 async function recipe() {
 	const api = await fetch(
 		`https://www.themealdb.com/api/json/v1/1/search.php?s=${inputDiv.value}`
 	);
+	if (!api.ok) {
+		swal(`No response from api${api.status}`);
+	}
 	const response = await api.json();
-	console.log(response);
-	imgContent.innerHTML = response.meals[0].strArea;
-	imgSpan.innerHTML = response.meals[0].strMeal;
-	cardImg.src = response.meals[0].strMealThumb;
+	if (!response.meals) {
+		content.innerHTML = "";
+		inputDiv.value = "";
+		swal("Not found", "the requested food name could not be found");
+	} else if (inputDiv.value === "") {
+		content.innerHTML = "";
+		swal("Please enter a food name !");
+	} else {
+		imgContent.innerHTML = response.meals[0].strArea;
+		imgSpan.innerHTML = response.meals[0].strMeal;
+		cardImg.src = response.meals[0].strMealThumb;
 
+		content.style.display = "block";
+		// Clear previous content in recipeContainer and recipeMeasure
+		recipeContainer.innerHTML = "";
 
-	// Clear previous content in recipeContainer and recipeMeasure
-	recipeContainer.innerHTML = "";
+		// Loop through ingredients and measures
+		for (let i = 1; i <= 20; i++) {
+			// Assuming you have up to 20 ingredients
+			const ingredient = response.meals[0][`strIngredient${i}`];
+			const measure = response.meals[0][`strMeasure${i}`];
 
-	// Loop through ingredients and measures
-	for (let i = 1; i <= 20; i++) {
-		// Assuming you have up to 20 ingredients
-		const ingredient = response.meals[0][`strIngredient${i}`];
-		const measure = response.meals[0][`strMeasure${i}`];
-
-		if (ingredient) {
-			const ingredientItem = document.createElement("li");
-			ingredientItem.innerHTML = `${measure} ${ingredient}`;
-			recipeContainer.appendChild(ingredientItem);
-            
+			if (ingredient) {
+				const ingredientItem = document.createElement("li");
+				ingredientItem.innerHTML = `${measure} ${ingredient}`;
+				recipeContainer.appendChild(ingredientItem);
+			}
 		}
 	}
 }
@@ -39,3 +56,12 @@ async function recipe() {
 searchBtn.addEventListener("click", recipe);
 // recipeContainer.innerHTML = "";
 // recipeMeasure.innerHTML = "";
+
+function viewRecipe() {
+	ins.style.display = "block";
+}
+
+recipeBtn.addEventListener("click", viewRecipe);
+closeBtn.addEventListener("click",()=>{
+	ins.style.display = "none"
+})
